588fab333ef7e64c75732349da3b846a7e9449b0
android-013 <azrifat013@gmail.com>
2025-03-03 22:33:15 +0600

    d2

--- scripts.js
+++ scripts.js
@@ -15,3 +15,19 @@ function display1(val) {
     }
 }
 
+function display2(val) {
+    if (check == 1) {
+        if (val == '0' || val == '1' || val == '2' || val == '3' || val == '4' || val == '5' || val == '6' || val == '7' || val == '8' || val == '9' || val == "(" || val == ")" || val == "sin(" || val == "cos(" || val == "tan(" || val == "log(" || val == "ln(" || val == "π" || val == "e" || val == "√(" || val == "abs(") {
+            clr();
+        }
+        else if (expression2 == "Error" || expression2 == "NaN") {
+            clr();
+        }
+        else {
+            check = 0;
+        }
+    }
+    var m = document.getElementById("expression1");
+    m.value += val;
+    scroll_right();
+}
