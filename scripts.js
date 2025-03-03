cdc335affbdd4bc5150cb7e2c932b5f83dfb1eb9
android-013 <azrifat013@gmail.com>
2025-03-03 22:37:09 +0600

    f1

--- scripts.js
+++ scripts.js
@@ -385,3 +385,26 @@ function solve() {
 }
 var temp = '';
 var check = 0;
+
+function checkBalancedParentheses(str) {
+    let stack = [];
+    let map = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === '(' || str[i] === '{' || str[i] === '[' ) {
+            stack.push(str[i]);
+        }
+        else if(str[i] === ')' || str[i] === '}' || str[i] === ']' ) {
+            let last = stack.pop();
+
+            if (str[i] !== map[last]) {return 0};
+        }
+    }
+    
+    if (stack.length !== 0) {return 1};
+    return 2;
+}
