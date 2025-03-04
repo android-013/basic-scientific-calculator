71f44a9a3885cfb1229c5cdb88b5d69f3db2edb0
android-013 <azrifat013@gmail.com>
2025-03-04 23:33:24 +0600

    f tan

--- scripts.js
+++ scripts.js
@@ -582,3 +582,20 @@ function cosfn() {
         gfg_Run();
     }
 }
+
+function tanfn() {
+    var x = document.getElementById("expression1").value;
+    var n = x.length - 1;
+    if (check == 0 && ((x[n] == ")" || x[n] == "!" || x[n] == "π" || x[n] == "e" || x[n] == "\u00B2" || x[n] == "%" || x[n] == 0 || x[n] == 1 || x[n] == 2 || x[n] == 3 || x[n] == 4 || x[n] == 5 || x[n] == 6 || x[n] == 7 || x[n] == 8 || x[n] == 9))) {
+        multiply();
+    }
+    display2('tan(');
+    display3('ttt(');
+    check = 0;
+    var h = document.getElementById("history");
+    var m = h.value.indexOf('Enter');
+    if (m < 0) {
+        h.value += "\n" + "Enter angle in degree.";
+        gfg_Run();
+    }
+}
