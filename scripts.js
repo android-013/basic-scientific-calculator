0da377a483cf6669d5ccae1a700eac3c2fb8a8b6
android-013 <azrifat013@gmail.com>
2025-03-04 23:29:34 +0600

    f pi

--- scripts.js
+++ scripts.js
@@ -485,3 +485,14 @@ function answer() {
     document.getElementById("expression1").value += temp;
     expression2 += temp;
 }
+
+function pi() {
+    var x = document.getElementById("expression1").value;
+    var n = x.length - 1;
+    if (check == 0 && ((x[n] == ")" || x[n] == "!" || x[n] == "π" || x[n] == "e" || x[n] == "\u00B2" || x[n] == "%" || x[n] == 0 || x[n] == 1 || x[n] == 2 || x[n] == 3 || x[n] == 4 || x[n] == 5 || x[n] == 6 || x[n] == 7 || x[n] == 8 || x[n] == 9))) {
+        multiply();
+    }
+    display2('π');
+    display3(Math.PI);
+    check = 0;
+}
